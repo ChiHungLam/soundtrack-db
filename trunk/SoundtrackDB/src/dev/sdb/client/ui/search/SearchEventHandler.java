23dbea5c24404bda3fc61f80e0023cf40a8f4817
larry.jooce@googlemail.com <larry.jooce@googlemail.com@558edeae-1894-bad6-e7c6-8edc70c67739>
2013-07-15 06:55:59 +0000

    Major refactoring. Switching to straight mvp design pattern.

--- trunk/SoundtrackDB/src/dev/sdb/client/ui/search/SearchEventHandler.java
+++
@@ -1,7 +0,0 @@
-package dev.sdb.client.ui.search;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface SearchEventHandler extends EventHandler {
-	void onSearch(SearchEvent event);
-}
