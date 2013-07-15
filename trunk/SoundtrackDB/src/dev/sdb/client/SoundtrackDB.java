45cb6d52efa2a395d93d70985cbb12e3ca531734
larry.jooce@googlemail.com <larry.jooce@googlemail.com@558edeae-1894-bad6-e7c6-8edc70c67739>
2013-07-15 21:04:16 +0000

    Minor changes on history handling.

--- trunk/SoundtrackDB/src/dev/sdb/client/SoundtrackDB.java
+++ trunk/SoundtrackDB/src/dev/sdb/client/SoundtrackDB.java
@@ -1,12 +1,9 @@
 package dev.sdb.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.RootPanel;
-
-import dev.sdb.client.view.UiFactory;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -23,16 +20,14 @@ public class SoundtrackDB implements EntryPoint {
 	public void onModuleLoad() {
 		System.out.println("Start up url: " + Window.Location.getHref());
 
-		// Create ClientFactory using deferred binding so we can replace with different
-		// impls in gwt.xml
-		UiFactory uiFactory = GWT.create(UiFactory.class);
-		ClientFactory clientFactory = new ClientFactory(uiFactory);
+		//Create the client factory
+		ClientFactory clientFactory = new ClientFactory();
 
-		//attaching navigator
-		IsWidget navigatorWidget = uiFactory.getNavigatorView();
-		RootPanel.get("navigator_area").add(navigatorWidget);
+		//Attach the navigator to the corresponding panel
+		IsWidget navigator = clientFactory.getUi().getNavigatorView();
+		RootPanel.get("navigator_area").add(navigator);
 
-		//Setting up history and displayed content
+		//Setting up history and display the current content
 		clientFactory.getHistoryManager().setUp();
 	}
 
