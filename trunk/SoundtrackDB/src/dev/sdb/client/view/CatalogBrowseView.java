a96a1a769a325fe9531e5706d4aeaf949f4cd421
larry.jooce@googlemail.com <larry.jooce@googlemail.com@558edeae-1894-bad6-e7c6-8edc70c67739>
2013-08-25 06:02:25 +0000

    Updated Soundtrack detail ui. Renaming CatalogTreeView interface and its CatalogTreeWidget implementation to CatalogBrowseView and CatalogBrowseWidget.

--- trunk/SoundtrackDB/src/dev/sdb/client/view/CatalogBrowseView.java
+++ trunk/SoundtrackDB/src/dev/sdb/client/view/CatalogBrowseView.java
@@ -9,12 +9,12 @@ import dev.sdb.shared.model.db.Result;
 import dev.sdb.shared.model.entity.Catalog;
 import dev.sdb.shared.model.entity.Entity;
 
-public interface CatalogTreeView extends IsWidget {
+public interface CatalogBrowseView extends IsWidget {
 
 	public interface Presenter {
 		void getCatalogLevelEntries(long parentId, AsyncDataProvider<Entity> dataProvider);
 
-		void getCatalogReleases(Catalog catalog, Range range, CatalogTreeView view);
+		void getCatalogReleases(Catalog catalog, Range range, CatalogBrowseView view);
 	}
 
 	void showResult(Result catalogResult);
